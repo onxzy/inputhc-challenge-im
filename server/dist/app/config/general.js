8f65c369688dde7afc9f19715884d50574317dda
onxzy <onxzycraft@gmail.com>
2023-01-11 14:53:25 +0100

    remove dist

--- server/dist/app/config/general.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    auth: {
-        verificationTokenExpiration: 24 * 60 * 60,
-        recoverTokenExpiration: 60 * 60,
-        recoverFormPath: '/login/recover',
-    },
-};
